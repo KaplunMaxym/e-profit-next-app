f6d5758afab58886abe0b7aceff4cf71ca6a7a65
Seva <vsev.diachun2002@gmail.com>
2024-01-02 15:36:07 +0200

    remove input type number, added page Permissions

--- src/UI/Inputs/digitCode/digitCode.tsx
+++ src/UI/Inputs/digitCode/digitCode.tsx
@@ -1,15 +1,25 @@
+'use client';
 import s from '@/UI/Inputs/digitCode/digitCode.module.scss'
-import type {FC} from "react";
+import {ChangeEvent, FC, useState} from "react";
 interface IInput {
     name?: string;
 }
 const DigitCode: FC<IInput> = ({name}) => {
+    const [value, setValue] = useState('');
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
+        const inputValue = e.target.value;
+        const maxValue = 999999;
+        if (inputValue === '' || (parseInt(inputValue) <= maxValue && inputValue.length <= 6)) {
+            setValue(inputValue);
+        }
+    }
     return <input
         className={s.input}
-        type={"text"}
+        type={"number"}
         name={name}
         required
-        maxLength={6}
+        value={value}
+        onChange={handleInputChange}
     />
 };
 
