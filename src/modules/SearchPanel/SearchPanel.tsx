e9a3cd5958d019a965e8f5462f47aec3c92691a9
KaplunMaxym <wertwertwert106@gmail.com>
2024-01-05 13:29:51 +0200

    custom location selects

--- src/modules/SearchPanel/SearchPanel.tsx
+++ src/modules/SearchPanel/SearchPanel.tsx
@@ -11,22 +11,20 @@ const city = [
         city: 'Lviv'
     },
 ]
-const area = [
-    {
-        id: 1,
-        city: 'Bereshany'
-    },
-    {
-        id: 2,
-        city: 'Terebovlia'
-    },
-]
+const area: any[] = [];
+
+for (let i = 1; i <= 50; i++) {
+    area.push({
+        id: i,
+        city: `City${i}`
+    });
+}
 const SearchPanel = () => {
     return (
         <div className={s.container}>
             <Search/>
-            <SelectorLocation data={area} />
-            {/*<SelectorLocation data ={city} />*/}
+            <SelectorLocation data={area} headerTitle={'Виберіть область'}/>
+            <SelectorLocation data={city} headerTitle={'Виберіть місто'} />
             <BtnLink height100 yellow fontSize={20} href={'/'}>Знайти роботу</BtnLink>
         </div>
     );
