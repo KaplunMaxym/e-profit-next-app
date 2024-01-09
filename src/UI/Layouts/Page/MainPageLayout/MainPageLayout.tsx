e924da9d55bb5c4a11589dea06479d93a0880e31
KaplunMaxym <wertwertwert106@gmail.com>
2024-01-09 16:02:22 +0200

    fixed previous commit v2

--- src/UI/Layouts/Page/MainPageLayout/MainPageLayout.tsx
+++ src/UI/Layouts/Page/MainPageLayout/MainPageLayout.tsx
@@ -1,5 +1,5 @@
 import {FC, ReactNode} from "react";
-import s from '@/UI/Layouts/Page/DefaultPage/pageDefault.module.scss'
+import s from '@/UI/Layouts/Page/MainPageLayout/MainPageLayout.module.scss'
 
 interface IDefaultPage {
     children: ReactNode;
